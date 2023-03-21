fae489915e3915a636e6f3c44ae395ac64b1667a
Pedro Mendes <pedromendes.developer@gmail.com>
2023-03-21 14:20:14 -0300

    Bug/fix: paint.js

--- pages/paint.js
+++ pages/paint.js
@@ -5,8 +5,7 @@ import Canvas from "components/canvas";
 import PromptForm from "components/prompt-form";
 import Dropzone from "components/dropzone";
 import Download from "components/download";
-import { XCircle as StartOverIcon } from "lucide-react";
-import { Code as CodeIcon, Rocket as RocketIcon } from "lucide-react";
+import { Code as CodeIcon, Rocket as RocketIcon, XCircle as StartOverIcon  } from "lucide-react";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
